2704af7a0a555e6f0ae7edf9a0e2f16c2b10cb8d
Moises <moisesroch2@gmail.com>
2026-05-17 02:51:04 -0300

    Criado a controller

---
+++ src/main/java/dev/java10xRocha/CadastroDeNinjasR/Controller.java
@@ -0,0 +1,14 @@
+package dev.java10xRocha.CadastroDeNinjasR;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+     return "Essa é a minha primeira mensasgem nessa rota";
+    }
+}
