3fdd9889dc6cda119e8a3c60a69d7838999983e3
Moises <moisesroch2@gmail.com>
2026-05-17 02:01:30 -0300

    first commit

---
+++ src/main/java/dev/java10xRocha/CadastroDeNinjasR/CadastroDeNinjasRApplication.java
@@ -0,0 +1,13 @@
+package dev.java10xRocha.CadastroDeNinjasR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10xRocha/CadastroDeNinjasR/CadastroDeNinjasRApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10xRocha.CadastroDeNinjasR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
